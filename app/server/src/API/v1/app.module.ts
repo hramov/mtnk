b64191486f320e7e778994108167c396c7e9f8d8
hramov <trykhramov@gmail.com>
2023-05-18 18:25:24 +0700

    fix module imports

--- app/server/src/API/v1/app.module.ts
+++ app/server/src/API/v1/app.module.ts
@@ -3,8 +3,9 @@ import {LoggerModule} from "./common/logger/logger.module";
 import {UserModule} from "./modules/user/user.module";
 import {TnkModule} from "./modules/tnk/tnk.module";
 import { DictionaryModule } from './modules/dictionary/dictionary.module';
+import { AihModule } from './modules/aih/aih.module';
 
 @Module({
-  imports: [LoggerModule, UserModule, TnkModule, DictionaryModule],
+  imports: [LoggerModule, UserModule, TnkModule, DictionaryModule, AihModule],
 })
 export class AppModule {}
